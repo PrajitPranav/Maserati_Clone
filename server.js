f7c2a74d593715553bf57c207a55b55428efa569
PRAJIT PRANAV K <pranavkarthi2t7@gmail.com>
2026-08-06 22:18:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -85,7 +85,6 @@ app.post('/api/auth/login', async (req, res) => {
     res.status(500).json({ message: 'Server error' });
   }
 });
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
